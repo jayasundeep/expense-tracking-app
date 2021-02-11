3ae9c8f8c466b8f3a05eba142efebd5e2bbd1c3f
jayasundeep <57098920+jayasundeep@users.noreply.github.com>
2021-02-11 13:44:25 +0530

    Update NotFoundpage.js

--- src/components/NotFoundpage.js
+++ src/components/NotFoundpage.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const NotFoundpage = () => (
     <div>
-        <div id="error" class="error_code_400">
+        <div id="error" class="error_code_404">
             <div class="error_message">
                 The request could not be understood.
                 <a href="/">Go to the homepage</a> »
             </div>
             <div class="error_bubble">
-                <div class="error_code">400<br><span>ERROR</span></div>
+                <div class="error_code">404<br><span>ERROR</span></div>
                 <div class="error_quote">Lord! It's a miracle! Webpage up and vanished like a fart in the wind!</div>
             </div>
             <div class="error_arrow"></div>
             <div class="error_attrib">
-                <span>Warden Norton, </span><a href="/title/tt0111161/">The Shawshank Redemption (1994)</a>
+                <span>Warden Norton, </span><a href="/">The Shawshank Redemption (1994)</a>
             </div>
             <div class="clear"></div>
         </div>
